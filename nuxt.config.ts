640e49c29e3e5848bd916a8a96a7fb4a5f1cca7f
Miles Wu <slion0725@gmail.com>
2024-07-23 14:22:45 +0800

    chore: update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
-  compatibilityDate: '2024-07-01',
 
   modules: [
     'nuxt-svgo-loader',
